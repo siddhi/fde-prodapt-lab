d13ca5416567c73d5fa5f723c7991ba3a62f57be
Tamizhvendan S <tamizh88@gmail.com>
2025-11-22 17:40:12 +0530

    add job boards

---
+++ frontend/app/routes/job_boards.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "../app/+types/root";
+
+export async function clientLoader() {
+  const res = await fetch(`/api/job-boards`);
+  const jobBoards = await res.json();
+  return {jobBoards}
+}
+
+export default function JobBoards({loaderData} : Route.ComponentProps) {
+  return (
+    <div>
+      {loaderData.jobBoards.map((jobBoard) => <p>{jobBoard.slug}</p>)}
+    </div>
+  )
+}
